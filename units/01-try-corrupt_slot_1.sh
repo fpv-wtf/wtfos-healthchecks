d86c8741d78a9ad5552e1dc4c53f5f295da3a21c
Joonas Trussmann <joonas@batman.ee>
2022-09-14 22:08:00 +0300

    Fixed issue with 01 and 02 interaction

--- units/01-try-corrupt_slot_1.sh
+++ units/01-try-corrupt_slot_1.sh
@@ -3,13 +3,14 @@
 #this script checks for a corrupt slot 1 and will reflash it if necessary and ota.zip is available
 check () {
     #check whatever our conditions is - this may be more complicated for other issues
-    if [ $(unrd slot_1.status_successful) != "1" ] && [ ! -f /data/wtfos_slot_attempt ]; then
+    if [ $(unrd slot_1.status_successful) != "1" ]; then
+        if [ ! -f /data/wtfos_slot_attempt ]; then
             echo "Slot 2 active, wtfos should be installed in Slot 1"
             return 2 
+        fi
     else
         if [ -f /data/wtfos_slot_attempt ] ; then
             rm /data/wtfos_slot_attempt
-            return 0
         fi
     fi
 }
